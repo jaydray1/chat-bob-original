f848015d8d6bac8fc78fda62a020e7af74dda9e8
Jon-Michael Dreher <jdreher15@gmail.com>
2021-12-07 13:54:39 -0700

    chat box styles.

--- client/src/ChatRoom/ChatRoom.tsx
+++ client/src/ChatRoom/ChatRoom.tsx
@@ -2,57 +2,137 @@ import React from 'react'
 import { useParams } from 'react-router';
 import useChat from '../hooks/useChat'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const ChatRoomContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
+    display: flex;
+    height: 400px;
+    padding: 1rem;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    background-color: #fff;
+    flex-direction: column-reverse;
+    overflow-y: auto;
 `
 
+const MessagesContainer = styled.div`
+    display: flex;
+    height: 400px;
+    padding: 1rem;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    background-color: #fff;
+    flex-direction: column;
+    overflow-y: auto;
+`
 
+const MessageItem = styled.div`
+    padding: 1rem 0 0 0;
+    transition: all 0.15s ease-in-out;
+    animation: fadeNewMessage 0.5s;
+    animation-fill-mode: forwards;
+`
+
+const MessageText = styled.div<{$ownedByCurrentUser: boolean}>`
+    float: right;
+    text-align: right;
+    margin: 0 1rem 0 0;
+    background-color: #fafafa;
+    ${({ $ownedByCurrentUser }) => $ownedByCurrentUser && `
+        float: left;
+        text-align: left;
+        margin: 0 0 0 1rem;
+        color: #fff;
+        background-color: #4870df;
+    `}
+` 
+
+const TextInput = styled.input`
+    height: 40px;
+    border: none;
+    padding: 0.5rem 1rem;
+    border-radius: 40px;
+    margin: 0 0.5rem 0 0;
+    width: calc(100% - 52px);
+    background-color: #dedfed;
+    box-shadow: inset 0 0 0 2px #dedfed;
+    font-size: 14px;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: bold;
+    transition: 0.15s all ease-in-out;
+    box-sizing: border-box;
+`
+
+const SendMessageButton = styled.div`
+  float: right;
+    position: relative;
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+    padding: 8px;
+    border-radius: 30px;
+    color: #fff;
+    background-color: #4870df;
+    text-align: center;
+    box-shadow: 0px 14px 10px -8px rgb(0 0 0 / 20%);
+    transition: 0.15s all ease-in-out;
+    box-sizing: border-box;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    svg {
+      font-size: 22px;
+      transform: rotate(60deg);
+    }
+`
 const ChatRoom = () => {
   const { rickId } = useParams()
-  const { messages, sendMessage } = useChat(rickId); // Creates a websocket and manages messaging
-  const [newMessage, setNewMessage] = React.useState(""); // Message to be sent
+  const { messages, sendMessage } = useChat(rickId)
+  const [newMessage, setNewMessage] = React.useState('')
 
-  const handleNewMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setNewMessage(event.target.value);
+  const handleNewMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewMessage(event.target.value)
   };
 
   const handleSendMessage = () => {
-    sendMessage(newMessage);
-    setNewMessage("");
+    sendMessage(newMessage)
+    setNewMessage('')
   };
 
   return (
-    <div className="chat-room-container">
-      <h1 className="room-name">Room: {rickId}</h1>
-      <div className="messages-container">
-        <ol className="messages-list">
-          {messages.map((message, i) => (
-            <li
-              key={i}
-              className={`message-item ${
-                message.ownedByCurrentUser ? "my-message" : "received-message"
-              }`}
-            >
-              {message.body}
-            </li>
-          ))}
-        </ol>
-      </div>
-      <textarea
-        value={newMessage}
-        onChange={handleNewMessageChange}
-        placeholder="Write message..."
-        className="new-message-input-field"
-      />
-      <button onClick={handleSendMessage} className="send-message-button">
-        Send
-      </button>
-    </div>
-  );
-};
+    <>
+      <ChatRoomContainer>
+        <h1>Rick Type: {rickId}</h1>
+        <MessagesContainer>
+            {messages.map((message, i) => (
+              <MessageItem key={i}>
+                <MessageText $ownedByCurrentUser={message.ownedByCurrentUser}>
+                  {message.body}
+                </MessageText>
+              </MessageItem>
+            ))}
+        </MessagesContainer>
+        </ChatRoomContainer>
+
+            <TextInput
+              type="text"
+              placeholder="Text message"
+              value={newMessage}
+              onChange={handleNewMessageChange}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSendMessage()
+                }
+              }}
+            />
+            <SendMessageButton onClick={handleSendMessage}>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </SendMessageButton>
+    </>
+  )
+}
 
 export default ChatRoom
